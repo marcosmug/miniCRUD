92ae8c2fb0b8081b3769caa13bcbfb97f350587b
marcosmug <94381046+marcosmug@users.noreply.github.com>
2022-04-22 11:14:21 +0200

    items

--- includes/itembox.php
+++
@@ -1,9 +0,0 @@
-<!-- <div class="itembox">
-    <div class="itembutton">
-        <button type="button" onclick="alert('Werkt nog niet :(')">Toevoegen aan winkel wagen</button>
-        <div class="itempic">
-            
-        </div>
-    </div>
-</div>
-dit is niet meer nodig -->
